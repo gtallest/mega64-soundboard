4b93d5db488c2dfed2f7add78a802b85da68372e
marc <marcmarcwongwong@gmail.com>
2016-09-27 23:52:03 -0700

    Adding Files

---
+++ data/board-data.js
@@ -0,0 +1,199 @@
+var json = [{
+  "characterClass":"treCool",
+  "characterName":"Tre Cool",
+  "cast":"Rocco",
+  "audio": [
+      "./audio/no_diggity_doberman.mp3"
+  ],
+  "quote":"No Diggity Doberman!",
+  "source":"Podcast 371",
+  "sourceLink":"https://www.youtube.com/watch?v=ziHQUNZcVj4"
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+      "./audio/puu_puus.mp3"
+  ],
+  "quote":"I see PUU, you see PUUS. I guess we see- I guess we are what we eat.",
+  "source":"Podcast 324: will.i.am idiot.he.is - PULS",
+  "sourceLink":"https://www.youtube.com/watch?v=efnYSfT574M"
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+    "./audio/clayvoice.mp3"
+  ],
+  "quote":"You have killed yourself by your own actions!",
+  "source":"Podcast 347: Joker Voice",
+  "sourceLink":"https://www.youtube.com/watch?v=_bsR1T9RDGw"
+},
+{
+  "characterClass":"newBatman",
+  "characterName":"New Batman",
+  "cast":"Derrick",
+  "audio": [
+    "./audio/no_loitering.mp3"
+  ],
+  "quote":"NO LOITERING!",
+  "source":"THE NEW BATMAN: VENGEANCE",
+  "sourceLink":"https://www.youtube.com/watch?v=R_Y_wvY0Kdg"
+},
+{
+  "characterClass":"dogBountyHunter",
+  "characterName":"Dog, the Bounty Hunter",
+  "cast":"Garrett",
+  "audio": [
+    "./audio/kid_butt_lover.mp3"
+  ],
+  "quote":"Die little kid butt lover!",
+  "source":"METAL GEAR SOLID V SPOOF",
+  "sourceLink":"https://www.youtube.com/watch?v=oORxO1LGNCg"
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+    "./audio/just_a_skit_1.mp3"
+  ],
+  "quote":"IT'S JUST A SKIT",
+  "source":"Podcast 188: Megan Lee Heart",
+  "sourceLink":"https://www.youtube.com/watch?v=ViaSCEYgWIA"
+},
+{
+  "characterClass":"sixtyFour",
+  "characterName":"Derrick",
+  "cast":"Derrick",
+  "audio": [
+    "./audio/kings_of_leon.mp3"
+  ],
+  "quote":"Kings of Leon: Ooooh ah oooooooohh uhhhhhhahhhuhhh ahahhhuhhhhhh",
+  "source":"Podcast 362",
+  "sourceLink":"https://www.youtube.com/watch?v=yXN-LSuXw5M"
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+},
+{
+  "characterClass":"Rocco_1",
+  "characterName":"Rocco",
+  "cast":"Rocco",
+  "audio": [
+
+  ],
+  "quote":"We are what we eat",
+  "source":"",
+  "sourceLink":""
+}
+];
---
+++ js/board.js
@@ -0,0 +1,146 @@
+$(document).ready(function(){
+
+  var boardHTML = "";
+  var quoteArray = [];
+  var charArray = [];
+  var quotesAndChars;
+  var noAudio = '';
+
+  for(var j in json){
+
+    if(json[j].audio.length === 0){
+      noAudio = '<div class="noAudio"><i class="fa fa-volume-up"></i><div class="audioSlash"></div></div>';
+    }
+
+    var portraitHTML = '<li>      <div class="portrait-image ' + json[j].characterClass + ' ' + json[j].cast + '">      </div>      <div class="tool-tip">' + json[j].characterName + '<div class="triangle">        </div>      </div>      <div class="speech-bubble"><span class="quote-text">"' + json[j].quote + '"</span><span class="quote-divider"></span><span class="quote-source">-<a href="' + json[j].sourceLink + '" target="_blank">' + json[j].source + '</a></span></div> ' + noAudio + '     <audio preload="none">      <source src="' + json[j].audio[0] + '" type="audio/mp3"/></audio>    </li>\n';
+
+    boardHTML += portraitHTML;
+
+    quoteArray.push(json[j].quote);
+    if(charArray.indexOf(json[j].characterName) > -1){
+
+    }
+    else {
+      charArray.push(json[j].characterName);
+    }
+
+  }
+
+  quotesAndChars = quoteArray.concat(charArray);
+
+  $('#board-list').html(boardHTML);
+
+  // $('.portrait-image').on('mouseover',function(){
+  //   $('.speech-bubble', $(this).parentNode).css('opacity','1');
+  // });
+  // $('.portrait-image').on('mouseout',function(){
+  //   $('.speech-bubble', $(this).parentNode).css('opacity','0');
+  // });
+
+
+
+  $('.portrait-image').on('click',function(){
+    var audio = this.parentNode.getElementsByTagName('audio')[0];
+    audio.play();
+  });
+
+  $('.quote-filter').on('click',function(){
+    $(this).toggleClass('filter-inactive');
+  });
+
+  $('.rocco-filter').on('click',function(){
+    $('.Rocco').parent().toggle();
+  });
+  $('.derrick-filter').on('click',function(){
+    $('.Derrick').parent().toggle();
+  });
+  $('.garrett-filter').on('click',function(){
+    $('.Garrett').parent().toggle();
+  });
+
+$('#filter-highlight').css('left', $('#search-glass').position().left + 2);
+
+$('#filter-icon').on('click',function(){
+  if(!$(this).hasClass('filter-active')){
+    $(this).toggleClass('filter-active');
+    $('#search-glass').toggleClass('filter-active');
+  }
+  $('#board-list li').show();
+  $('.quote-filter').removeClass('filter-inactive');
+  $('#search input').val('');
+  $('#search input').keyup();
+  $('#filter-highlight').css('left', $('#filter-icon').position().left);
+  $('#search').slideUp((function(){
+    $('#filter-list').slideDown();
+  }));
+});
+$('#search-glass').on('click',function(){
+  if(!$(this).hasClass('filter-active')){
+    $(this).toggleClass('filter-active');
+    $('#filter-icon').toggleClass('filter-active');
+  }
+  $('#board-list li').show();
+  $('.quote-filter').removeClass('filter-inactive');
+  $('#filter-highlight').css('left', $('#search-glass').position().left + 1);
+  $('#filter-list').slideUp(function(){
+    $('#search').slideDown();
+  });
+});
+
+
+//Search Filter
+$quotes = $('#board-list li');
+$('#search input').keyup(function(){
+  var val = '^(?=.*\\b' + $.trim($(this).val()).split(/:*-*,*\s+/).join('\\b)(?=.*\\b') + ').*$',
+        reg = RegExp(val, 'i'),
+        text;
+console.log('val: ' + val);
+    $quotes.show().filter(function() {
+        text = $(this).text().replace(/\s+/g, ' ');
+        return !reg.test(text);
+    }).hide();
+});
+
+//Clear
+$('#search-clear').on('click',function(){
+  $('#search input').val('');
+  $('#search input').keyup();
+  $('#search-clear').fadeOut();
+});
+
+$('#search input').focus(function(){
+  $('#search-clear').fadeIn();
+}).blur(function(){
+  if($(this).val() !== ''){
+
+  }
+  else {
+    $('#search-clear').fadeOut();
+  }
+});
+
+//Auto Complete?
+var ac = new autoComplete({
+    selector: 'input[name="search"]',
+    minChars: 1,
+    source: function(term, suggest){
+        term = term.toLowerCase();
+        var choices = quotesAndChars;
+        var matches = [];
+        for (i=0; i<choices.length; i++)
+            if (~choices[i].toLowerCase().indexOf(term)) matches.push(/*"\"" + */choices[i]/*.toUpperCase() + "\""*/);
+        suggest(matches);
+    },
+    onSelect: function(e, term, item){
+      $('#search input').keyup();
+    }
+});
+
+
+
+
+  var slide = function() {
+    $('#board-list').slideDown();
+  };
+  setTimeout(slide,500);
+});
